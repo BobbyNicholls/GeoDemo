7e08edcdc26d11523146f5bfc6438d059cb32420
tsameti <robert.r.nicholls@gmail.com>
2024-06-05 20:59:38 +0100

    added model deployment code

---
+++ deploy_geo_model.py
@@ -0,0 +1,19 @@
+import boto3
+import sagemaker
+from sagemaker import get_execution_role
+from sagemaker.model import Model
+
+region_name = "eu-west-2"
+role = get_execution_role()
+session = sagemaker.Session(boto3.Session(region_name=region_name))
+model_artifact = "s3://time-series-geo-data/model-output/sagemaker-xgboost-2024-06-05-19-21-30-670/output/model.tar.gz"
+container = sagemaker.image_uris.retrieve("xgboost", region_name, version="1.2-1")
+
+model = Model(
+    image_uri=container, model_data=model_artifact, role=role, sagemaker_session=session
+)
+predictor = model.deploy(
+    initial_instance_count=1,
+    instance_type="ml.t2.medium",
+    endpoint_name="geo-model-endpoint1",
+)
