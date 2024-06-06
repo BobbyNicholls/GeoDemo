1c96992ed5966f6a6ca85a0faf2347c08d16f31c
tsameti <robert.r.nicholls@gmail.com>
2024-06-06 16:34:34 +0100

    added code to invoke the model endpoint

---
+++ machine_learning/invoke_geo_model.py
@@ -0,0 +1,16 @@
+import boto3
+
+sagemaker_client = boto3.client('sagemaker-runtime')
+s3_client = boto3.client("s3")
+request_data = (
+    s3_client.get_object(Bucket="time-series-geo-data", Key="request_sample.libsvm")["Body"]
+    .read()
+    .decode("utf-8")
+)
+response = sagemaker_client.invoke_endpoint(
+    EndpointName="geo-model-endpoint",
+    ContentType='text/libsvm',
+    Body=request_data
+)
+result = response['Body'].read().decode('utf-8')
+print(f"Prediction: {result}")
