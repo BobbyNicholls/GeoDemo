b870f91f38a4c70734b3d5bac4bb024d8a67e245
tsameti <robert.r.nicholls@gmail.com>
2024-06-06 16:40:40 +0100

    removed redundant imports, minor code clean up

--- machine_learning/train_geo_model.py
+++ machine_learning/train_geo_model.py
@@ -1,10 +1,8 @@
 import boto3
 import sagemaker
-from sagemaker import get_execution_role
-from sagemaker.amazon.amazon_estimator import get_image_uri
 
 session = sagemaker.Session(boto3.Session(region_name="eu-west-2"))
-role = get_execution_role()
+role = sagemaker.get_execution_role()
 session = sagemaker.Session()
 container = sagemaker.image_uris.retrieve(
     framework="xgboost", region=session.boto_region_name, version="1.2-1"
@@ -18,6 +16,5 @@ xgb = sagemaker.estimator.Estimator(
     output_path="s3://time-series-geo-data/model-output",
     sagemaker_session=session,
 )
-
 xgb.set_hyperparameters(objective="reg:squarederror.", num_round=3)
 xgb.fit({"train": "s3://time-series-geo-data/bustard.libsvm"})
