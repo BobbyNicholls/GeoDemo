a80e2833b2086f136d331d3fb285b7931401fd06
tsameti <robert.r.nicholls@gmail.com>
2024-06-16 20:54:30 +0100

    updated app to take a date from the user and filter the geo_data to that date

--- app/map_app.py
+++ app/map_app.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 from io import StringIO
 from typing import Dict
 
@@ -11,7 +12,7 @@ from utils.geo_animation import get_time_stamped_geo_json
 
 
 def get_geo_data(
-    payload: Dict[str, float],
+    payload: Dict[str, str],
     url: str = "https://2gs5g97rxd.execute-api.eu-west-2.amazonaws.com/sandbox",
 ):
     response = requests.post(url, data=json.dumps(payload))
@@ -23,30 +24,21 @@ def get_geo_data(
 
 st.title("Simple Map App")
 st.sidebar.title("Input Lat/Longs:")
-origin_latitude = st.sidebar.text_input("Enter origin latitude:", "")
-origin_longitude = st.sidebar.text_input("Enter origin longitude:", "")
-destination_latitude = st.sidebar.text_input("Enter destination latitude:", "")
-destination_longitude = st.sidebar.text_input("Enter destination longitude:", "")
+selected_date = st.sidebar.date_input("Select a date", datetime.now())
+print(selected_date)
 
 # the_downs = [51.47168, -2.62186]
 # bristol = [51.4545, -2.5879]
 # germany_origin = [52.20031, 12.6059]
-# germany_destination = [52.18538, 12.67741]
+germany_destination = [52.18538, 12.67741]
 
 if st.sidebar.button("Submit"):
-    payload = {
-        "origin_latitude": origin_latitude,
-        "origin_longitude": origin_longitude,
-        "destination_latitude": destination_latitude,
-        "destination_longitude": destination_longitude,
-    }
-    geo_data = get_geo_data(payload)
-    origin = [geo_data["param1"], geo_data["param2"]]
-    destination = [geo_data["param3"], geo_data["param4"]]
+    payload = {"date": str(selected_date)}
+    geo_data = get_geo_data(payload).sort_values("UTC_timestamp")
+    start_latitude = geo_data["Latitude"]
+    start_longitude = geo_data["Longitude"]
     st.write("Interactive World Map")
-    folium_map = folium.Map(location=origin, zoom_start=10)
-    folium.Marker(origin, popup="Origin").add_to(folium_map)
-    folium.Marker(destination, popup="Destination").add_to(folium_map)
+    folium_map = folium.Map(location=[start_latitude, start_longitude], zoom_start=10)
     get_time_stamped_geo_json(pd.read_csv(StringIO(geo_data["geo_data"]))).add_to(
         folium_map
     )
