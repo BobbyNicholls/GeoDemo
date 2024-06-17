d7527a1dbae2acc17d19552496bdc6116457f4e9
tsameti <robert.r.nicholls@gmail.com>
2024-06-17 17:14:21 +0100

    move 'get_geo_data()' to its own file

--- app/map_app.py
+++ app/map_app.py
@@ -1,29 +1,9 @@
-import json
-from io import StringIO
-from typing import Dict
-
 import folium
 import pandas as pd
-import requests
 import streamlit as st
 from streamlit_folium import folium_static
+from utils.data_transfer import get_geo_data
 from utils.geo_animation import get_time_stamped_geo_json
-
-
-def get_geo_data(
-    payload: Dict[str, str],
-    url: str = "https://2gs5g97rxd.execute-api.eu-west-2.amazonaws.com/sandbox",
-    request_from_endpoint: bool = False,
-) -> pd.DataFrame:
-    if request_from_endpoint:
-        response = requests.post(url, data=json.dumps(payload))
-        if response.status_code == 200:
-            return pd.read_csv(StringIO(response.json()["geo_data"]))
-
-        raise ValueError(f"API failed with status code {response.status_code}")
-
-    return pd.read_csv("C:/dev/geo_birds/data/bustard.csv")
-
 
 st.title("Bustard Map App")
 st.sidebar.title("Input date you want to review:")
@@ -41,8 +21,10 @@ if st.sidebar.button("Submit"):
     if len(geo_data) > 0:
         start_latitude = geo_data["Latitude"].iloc[0]
         start_longitude = geo_data["Longitude"].iloc[0]
-        st.write("Interactive World Map")
-        folium_map = folium.Map(location=[start_latitude, start_longitude], zoom_start=16)
+        st.write(f"Movements on {selected_date}")
+        folium_map = folium.Map(
+            location=[start_latitude, start_longitude], zoom_start=16
+        )
         get_time_stamped_geo_json(geo_data).add_to(folium_map)
         folium_static(folium_map, width=1050, height=750)
     else:
